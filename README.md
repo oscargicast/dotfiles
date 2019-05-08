92fed15d6df2d05720f9fbf6e0a993ac7dc5f916
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2019-05-08 06:19:04 -0400

    [2] Closed. Add brew.sh

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ```
 git clone git@github.com:oskargicast/dotfiles.git && cd dotfiles && source bootstrap.sh
 ```
+
+If this is your fisrt time, you need to install brew dependencies.
+```
+source brew.sh
+```
---
+++ brew.sh
@@ -0,0 +1,10 @@
+brew update 
+
+brew install git 
+brew install git-flow
+brew install tmux 
+brew install wget
+brew install tree
+brew tap homebrew/cask-fonts
+brew cask install font-fira-code
+brew install terminal-notifier
